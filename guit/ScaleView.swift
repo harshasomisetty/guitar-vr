7e9178011b6a9176f5d80e38b21cd2ca76beb2c4
yasmeen <yasmeenroumie@gmail.com>
2024-02-24 16:28:21 -0500

    add fret board

--- guit/ScaleView.swift
+++ guit/ScaleView.swift
@@ -23,6 +23,92 @@ let scales = [
     Scale(name: "Pentatonic", notes: ["C", "D", "E", "G", "A"]),
 ]
 
+// Visualize fret board
+let strings = ["E", "A", "D", "G", "B", "E"]
+
+// Show the notes on the fret board
+// The fretMatrix is a 2D array of strings, where the first index is the string number and the second index is note letter
+let fretMatrix = [
+    ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E"],
+    ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"],
+    ["D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D"],
+    ["G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G"],
+    ["B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
+    ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E"],
+]
+
+// Visualize the scale on the fret board
+// Display six strings and highlight the selected note
+// The fretboard should be a grid of lines and circles
+// Only circle the first instance of the note on the fretboard
+struct FretBoard: View {
+    var scale: Scale
+    var scaleNoteIndex: Int
+    
+    private func firstOccurrenceOfNote() -> (stringIndex: Int, fretIndex: Int)? {
+            let targetNote = scale.notes[scaleNoteIndex]
+
+            for (stringIndex, string) in fretMatrix.enumerated() {
+                if let fretIndex = string.firstIndex(of: targetNote) {
+                    return (stringIndex, fretIndex+1) // Return the first occurrence found
+                }
+            }
+            return nil // No occurrence found
+        }
+
+        var body: some View {
+            let firstOccurrence = firstOccurrenceOfNote() // Calculate this outside of the View's body
+
+            VStack {
+                ForEach(0..<7, id: \.self) { stringIndex in
+                    HStack {
+                        ForEach(0..<13, id: \.self) {
+                            fretIndex in
+                            
+                            if (stringIndex == 6) {
+                                if (fretIndex == 0) {
+                                    Circle()
+                                        .strokeBorder(Color.black, lineWidth: 0)
+                                        .background(Circle().fill(Color.clear))
+                                        .frame(width: 20, height: 20)
+                                } else {
+                                    // render a bunch of circles with a number in each
+                                    Circle()
+                                        .strokeBorder(Color.black, lineWidth: 0)
+                                        .background(Circle().fill(Color.black))
+                                        .frame(width: 20, height: 20)
+                                        .overlay(
+                                            Text("\(fretIndex)").foregroundColor(Color.white)
+                                        )
+                                }
+                            } else {
+                                if (fretIndex == 0) {
+                                    // Render circles with the note of the string
+                                    Circle()
+                                        .strokeBorder(Color.black, lineWidth: 1)
+                                        .background(Circle().fill(Color.black))
+                                        .frame(width: 20, height: 20)
+                                        .overlay(
+                                            Text("\(strings[stringIndex])").foregroundColor(Color.white)
+                                        )
+                                } else {
+                                    Circle()
+                                        .strokeBorder(Color.black, lineWidth: 1)
+                                        .background((firstOccurrence?.stringIndex == stringIndex && firstOccurrence?.fretIndex == fretIndex) ? Circle().fill(Color.blue) : Circle().fill(Color.clear))
+                                        .frame(width: 20, height: 20)
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            
+            
+        }
+}
+
 // Show the scale name on top
 // Then start with the first note of the scale large in the center, and when the user taps on it, move to the next note
 
@@ -52,9 +138,10 @@ struct ScaleDetail: View {
                     }
                 
                 Spacer()
-            }.gesture(TapGesture().onEnded { _ in
-                scaleNoteIndex = (scaleNoteIndex + 1) % scale.notes.count
-            })
+                
+                FretBoard(scale: scale, scaleNoteIndex: scaleNoteIndex)
+                Spacer()
+            }
         }
 }
 
